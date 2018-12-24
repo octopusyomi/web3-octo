eb549582c72ff3f2e69b4df8b80df0202ccbd4f5
miraiyo <funyloony@gmail.com>
2018-12-24 10:38:59 +0300

    Initial commit

---
+++ lib/eth/octo/version.rb
@@ -0,0 +1,5 @@
+module Ethereum
+  module Octopus
+    VERSION = '0.1.0'
+  end
+end
---
+++ spec/ethereum/octopus_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Ethereum::Octopus do
+  it "has a version number" do
+    expect(Ethereum::Octopus::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
